8f12c56e4132e8f8bc61a404e17020070c206a06
samyusuf101 <samyusuf101@gmail.com>
2025-09-24 10:49:22 +0100

    edited DeployMyToken.sol

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -2,11 +2,10 @@
 
 pragma solidity ^0.8.18;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20{
+contract MyToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Zilla", "Zil") {
-        _mint (msg.sender, initialSupply);
-        
+        _mint(msg.sender, initialSupply);
     }
-   
 }
